030fcfaf9dda3e90525dc8e1c43c3a2bb5cefee4
li.rongping <li.rongping@immomo.com>
2021-08-06 19:12:47 +0800

    init

---
+++ shell/platform/darwin/ios/framework/Source/FlutterAppDelegateTest.mm
@@ -0,0 +1,97 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <OCMock/OCMock.h>
+#import <XCTest/XCTest.h>
+
+#import "flutter/shell/platform/darwin/ios/framework/Headers/FlutterAppDelegate.h"
+#import "flutter/shell/platform/darwin/ios/framework/Headers/FlutterEngine.h"
+#import "flutter/shell/platform/darwin/ios/framework/Headers/FlutterViewController.h"
+#import "flutter/shell/platform/darwin/ios/framework/Source/FlutterAppDelegate_Test.h"
+#import "flutter/shell/platform/darwin/ios/framework/Source/FlutterEngine_Test.h"
+
+FLUTTER_ASSERT_ARC
+
+@interface FlutterAppDelegateTest : XCTestCase
+@end
+
+@implementation FlutterAppDelegateTest
+
+- (void)testLaunchUrl {
+  FlutterAppDelegate* appDelegate = [[FlutterAppDelegate alloc] init];
+  FlutterViewController* viewController = OCMClassMock([FlutterViewController class]);
+  FlutterEngine* engine = OCMClassMock([FlutterEngine class]);
+  FlutterMethodChannel* navigationChannel = OCMClassMock([FlutterMethodChannel class]);
+  OCMStub([engine navigationChannel]).andReturn(navigationChannel);
+  OCMStub([viewController engine]).andReturn(engine);
+  // Set blockNoInvoker to a strong local to retain to end of scope.
+  id blockNoInvoker = [OCMArg invokeBlockWithArgs:@NO, nil];
+  OCMStub([engine waitForFirstFrame:3.0 callback:blockNoInvoker]);
+  appDelegate.rootFlutterViewControllerGetter = ^{
+    return viewController;
+  };
+  NSURL* url = [NSURL URLWithString:@"http://myApp/custom/route?query=test"];
+  NSDictionary<UIApplicationOpenURLOptionsKey, id>* options = @{};
+  BOOL result = [appDelegate application:[UIApplication sharedApplication]
+                                 openURL:url
+                                 options:options
+                         infoPlistGetter:^NSDictionary*() {
+                           return @{@"FlutterDeepLinkingEnabled" : @(YES)};
+                         }];
+  XCTAssertTrue(result);
+  OCMVerify([navigationChannel invokeMethod:@"pushRoute" arguments:@"/custom/route?query=test"]);
+}
+
+- (void)testLaunchUrlWithQueryParameterAndFragment {
+  FlutterAppDelegate* appDelegate = [[FlutterAppDelegate alloc] init];
+  FlutterViewController* viewController = OCMClassMock([FlutterViewController class]);
+  FlutterEngine* engine = OCMClassMock([FlutterEngine class]);
+  FlutterMethodChannel* navigationChannel = OCMClassMock([FlutterMethodChannel class]);
+  OCMStub([engine navigationChannel]).andReturn(navigationChannel);
+  OCMStub([viewController engine]).andReturn(engine);
+  // Set blockNoInvoker to a strong local to retain to end of scope.
+  id blockNoInvoker = [OCMArg invokeBlockWithArgs:@NO, nil];
+  OCMStub([engine waitForFirstFrame:3.0 callback:blockNoInvoker]);
+  appDelegate.rootFlutterViewControllerGetter = ^{
+    return viewController;
+  };
+  NSURL* url = [NSURL URLWithString:@"http://myApp/custom/route?query=test#fragment"];
+  NSDictionary<UIApplicationOpenURLOptionsKey, id>* options = @{};
+  BOOL result = [appDelegate application:[UIApplication sharedApplication]
+                                 openURL:url
+                                 options:options
+                         infoPlistGetter:^NSDictionary*() {
+                           return @{@"FlutterDeepLinkingEnabled" : @(YES)};
+                         }];
+  XCTAssertTrue(result);
+  OCMVerify([navigationChannel invokeMethod:@"pushRoute"
+                                  arguments:@"/custom/route?query=test#fragment"]);
+}
+
+- (void)testLaunchUrlWithFragmentNoQueryParameter {
+  FlutterAppDelegate* appDelegate = [[FlutterAppDelegate alloc] init];
+  FlutterViewController* viewController = OCMClassMock([FlutterViewController class]);
+  FlutterEngine* engine = OCMClassMock([FlutterEngine class]);
+  FlutterMethodChannel* navigationChannel = OCMClassMock([FlutterMethodChannel class]);
+  OCMStub([engine navigationChannel]).andReturn(navigationChannel);
+  OCMStub([viewController engine]).andReturn(engine);
+  // Set blockNoInvoker to a strong local to retain to end of scope.
+  id blockNoInvoker = [OCMArg invokeBlockWithArgs:@NO, nil];
+  OCMStub([engine waitForFirstFrame:3.0 callback:blockNoInvoker]);
+  appDelegate.rootFlutterViewControllerGetter = ^{
+    return viewController;
+  };
+  NSURL* url = [NSURL URLWithString:@"http://myApp/custom/route#fragment"];
+  NSDictionary<UIApplicationOpenURLOptionsKey, id>* options = @{};
+  BOOL result = [appDelegate application:[UIApplication sharedApplication]
+                                 openURL:url
+                                 options:options
+                         infoPlistGetter:^NSDictionary*() {
+                           return @{@"FlutterDeepLinkingEnabled" : @(YES)};
+                         }];
+  XCTAssertTrue(result);
+  OCMVerify([navigationChannel invokeMethod:@"pushRoute" arguments:@"/custom/route#fragment"]);
+}
+
+@end
